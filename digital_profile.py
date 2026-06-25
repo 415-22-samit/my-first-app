bf41754f316badd7eb66521d3a40988746a195f4
415-22-samit <56336@yupparaj.ac.th>
2026-06-25 09:10:16 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title(" My Digital Profile")
+st.header("เเนะนำตัว")
+st.write("นายสามิต ตามี่")
+st.write("ชื่อเล่น อาจือ")
+st.write("เลขที่ 22 ม.4/15")
+st.header("เป้าหมายเเละสิ่งที่อยากทำในอนาคต")
+st.write("เป้าหมาย: **อยากเรียนด้านคอมพิวเตอร์เเละเทคโนโลยี")
+st.write("สิ่งที่อยากเป็นในอนาคต: เทรดเดอร์ (Trader)")
